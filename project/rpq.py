711a5d47c7af45cf3a05ae3dcf94bbdd12f78842
Kirill Shishin <shishin_ka@mail.ru>
2024-09-30 18:22:07 +0300

    feat: Implement `ms_bfs_based_rpq`

--- project/rpq.py
+++ project/rpq.py
@@ -1,7 +1,10 @@
 import networkx as nx
+import numpy as np
+from scipy import sparse
 from project.adjacency_matrix_fa import AdjacencyMatrixFA, intersect_automata
 from project.graph_utils import graph_to_nfa
 from project.regex_utils import regex_to_dfa
+from project.vector_utils import create_bool_vector
 
 
 def tensor_based_rpq(
@@ -42,5 +45,104 @@ def tensor_based_rpq(
                         ]
 
                         res.add((res_start_state, res_final_state))
+
+    return res
+
+
+def ms_bfs_based_rpq(
+    regex: str, graph: nx.MultiDiGraph, start_nodes: set[int], final_nodes: set[int]
+) -> set[tuple[int, int]]:
+    regex_dfa = regex_to_dfa(regex)
+    dfa_adjacency_matrix_fa = AdjacencyMatrixFA(regex_dfa)
+
+    graph_nfa = graph_to_nfa(graph, start_nodes, final_nodes)
+    nfa_adjacency_matrix_fa = AdjacencyMatrixFA(graph_nfa)
+
+    k = dfa_adjacency_matrix_fa.states_number
+    n = nfa_adjacency_matrix_fa.states_number
+
+    dfa_start_state_ind = list(dfa_adjacency_matrix_fa.start_states_ind)[0]
+    nfa_start_states_ind = nfa_adjacency_matrix_fa.start_states_ind
+    nfa_start_states_number = len(nfa_start_states_ind)
+
+    def create_start_front():
+        data = np.ones(nfa_start_states_number, dtype=bool)
+        rows = [dfa_start_state_ind + k * j for j in range(nfa_start_states_number)]
+        columns = [start_state_ind for start_state_ind in nfa_start_states_ind]
+
+        return sparse.csr_matrix(
+            (data, (rows, columns)), shape=(k * nfa_start_states_number, n), dtype=bool
+        )
+
+    labels = (
+        dfa_adjacency_matrix_fa.boolean_decomposition_matrices.keys()
+        & nfa_adjacency_matrix_fa.boolean_decomposition_matrices.keys()
+    )
+
+    dfa_decomposed_matrices = dfa_adjacency_matrix_fa.boolean_decomposition_matrices
+    dfa_decomposed_transposed_matrices = dict(
+        zip(
+            dfa_decomposed_matrices.keys(),
+            map(lambda x: x.transpose(), dfa_decomposed_matrices.values()),
+        )
+    )
+
+    nfa_decomposed_matrices = nfa_adjacency_matrix_fa.boolean_decomposition_matrices
+
+    def update_front(front):
+        decomposed_fronts = {}
+        for label in labels:
+            decomposed_fronts[label] = front @ nfa_decomposed_matrices[label]
+
+            for ind in range(nfa_start_states_number):
+                decomposed_fronts[label][ind * k : (ind + 1) * k] = (
+                    dfa_decomposed_transposed_matrices[label]
+                    @ decomposed_fronts[label][ind * k : (ind + 1) * k]
+                )
+
+        new_front = sparse.csr_matrix((k * nfa_start_states_number, n), dtype=bool)
+
+        for decomposed_front in decomposed_fronts.values():
+            new_front += decomposed_front
+
+        return new_front
+
+    current_front = create_start_front()
+    visited = sparse.csr_matrix((k * nfa_start_states_number, n), dtype=bool)
+
+    while current_front.count_nonzero() > 0:
+        visited += current_front
+
+        current_front = update_front(current_front)
+
+        current_front = current_front > visited
+
+    dfa_final_states_ind = dfa_adjacency_matrix_fa.final_states_ind
+
+    res = set()
+
+    nfa_final_states_ind = nfa_adjacency_matrix_fa.final_states_ind
+    nfa_final_states_vector = create_bool_vector(
+        nfa_adjacency_matrix_fa.states_number, nfa_final_states_ind
+    )
+
+    for i, nfa_start_state_ind in enumerate(nfa_start_states_ind, 0):
+        for dfa_final_state_ind in dfa_final_states_ind:
+            row = visited.getrow(i * k + dfa_final_state_ind)
+            row_vector = create_bool_vector(n, row.indices)
+
+            vector = row_vector & nfa_final_states_vector
+
+            reached_nfa_final_states_ind = np.nonzero(vector)[0]
+
+            for reached_nfa_final_state_ind in reached_nfa_final_states_ind:
+                res.add(
+                    (
+                        nfa_adjacency_matrix_fa.ind_to_state[nfa_start_state_ind],
+                        nfa_adjacency_matrix_fa.ind_to_state[
+                            reached_nfa_final_state_ind
+                        ],
+                    )
+                )
 
     return res
