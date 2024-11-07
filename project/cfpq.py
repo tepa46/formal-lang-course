c49a1c9780e445ff1758731144d4d6c5bbba7821
Kirill Shishin <shishin_ka@mail.ru>
2024-11-07 18:14:50 +0200

    feat: Implement `tensor_based_cfpq` function

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,8 +1,13 @@
 from pyformlang.cfg import CFG, Terminal
+from pyformlang.rsa import RecursiveAutomaton
+
 import networkx as nx
 from scipy import sparse
 
+from project.graph_utils import graph_to_nfa
+from project.adjacency_matrix_fa import AdjacencyMatrixFA, intersect_automata
 from project.cfg_utils import cfg_to_weak_normal_form
+from project.rsm_utils import rsm_to_nfa
 
 
 def hellings_based_cfpq(
@@ -119,5 +124,89 @@ def matrix_based_cfpq(
                 and (not final_nodes or ind_to_node[j] in final_nodes)
             ):
                 result.add((ind_to_node[i], ind_to_node[j]))
+
+    return result
+
+
+def tensor_based_cfpq(
+    rsm: RecursiveAutomaton,
+    graph: nx.DiGraph,
+    start_states: set[int] = None,
+    final_states: set[int] = None,
+) -> set[tuple[int, int]]:
+    rsm_nfa = rsm_to_nfa(rsm)
+    rsm_adjacency_matrix_fa = AdjacencyMatrixFA(rsm_nfa)
+
+    graph_nfa = graph_to_nfa(graph, start_states, final_states)
+    graph_adjacency_matrix_fa = AdjacencyMatrixFA(graph_nfa)
+    m = graph_adjacency_matrix_fa.states_number
+
+    updated = True
+    while updated:
+        updated = False
+
+        intersect = intersect_automata(
+            rsm_adjacency_matrix_fa, graph_adjacency_matrix_fa
+        )
+
+        transitive_closure_matrix = intersect.get_transitive_closure_matrix()
+
+        for i, j in zip(*transitive_closure_matrix.nonzero()):
+            if not transitive_closure_matrix[i, j]:
+                continue
+
+            rsm_nfa_start_state_ind = i // m
+            _, rsm_nfa_start_label = rsm_adjacency_matrix_fa.ind_to_state[
+                rsm_nfa_start_state_ind
+            ].value
+
+            graph_nfa_start_state_ind = i % m
+
+            rsm_nfa_final_state_ind = j // m
+            _, rsm_nfa_final_label = rsm_adjacency_matrix_fa.ind_to_state[
+                rsm_nfa_final_state_ind
+            ].value
+
+            graph_nfa_final_state_ind = j % m
+
+            if rsm_nfa_start_label != rsm_nfa_final_label:
+                continue
+
+            if (
+                rsm_nfa_start_state_ind in rsm_adjacency_matrix_fa.start_states_ind
+                and rsm_nfa_final_state_ind in rsm_adjacency_matrix_fa.final_states_ind
+            ):
+                graph_adjacency_matrix_fa.boolean_decomposition_matrices.setdefault(
+                    rsm_nfa_start_label,
+                    sparse.csr_matrix((m, m), dtype=bool),
+                )
+
+                if not graph_adjacency_matrix_fa.boolean_decomposition_matrices[
+                    rsm_nfa_start_label
+                ][graph_nfa_start_state_ind, graph_nfa_final_state_ind]:
+                    graph_adjacency_matrix_fa.boolean_decomposition_matrices[
+                        rsm_nfa_start_label
+                    ][graph_nfa_start_state_ind, graph_nfa_final_state_ind] = True
+                    updated = True
+
+    if (
+        rsm.initial_label
+        not in graph_adjacency_matrix_fa.boolean_decomposition_matrices.keys()
+    ):
+        return set()
+
+    result = set()
+    for i in range(m):
+        for j in range(m):
+            i_state = graph_adjacency_matrix_fa.ind_to_state[i]
+            j_state = graph_adjacency_matrix_fa.ind_to_state[j]
+
+            if graph_adjacency_matrix_fa.boolean_decomposition_matrices[
+                rsm.initial_label
+            ][i, j]:
+                if (not start_states or i_state in start_states) and (
+                    not final_states or j_state in final_states
+                ):
+                    result.add((i_state, j_state))
 
     return result
